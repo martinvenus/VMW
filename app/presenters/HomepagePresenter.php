c1bb14d819a6f8601ad3989d6cc19ae83def1a4a
Martin Venuš <martin.venus@gmail.com>
2010-12-06 21:55:38 +0100

    VyvojX

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -14,32 +14,6 @@
  */
 class HomepagePresenter extends BasePresenter {
 
-    public function actionXXX() {
-        $f = new FlickrModel("eb01c6c4e23a0f036988692a7f42dd14");
-
-        $recent = $f->photos_search(array("tags" => "český krumlov", "tag_mode" => "any", "sort" => "relevance", "media" => "photos", "per_page" => 20));
-
-        foreach ($recent['photo'] as $key => $photo) {
-            $sizes = $f->photos_getSizes($photo['id']);
-
-            $isLarge = false;
-            foreach ($sizes as $size) {
-                if (strcmp($size['label'], 'Large') == 0) {
-                    $isLarge = true;
-                    break;
-                }
-            }
-
-            if ($isLarge == false) {
-
-                //$this->redirect('Homepage:default', ($page + 1));
-                unset($recent['photo'][$key]);
-            }
-        }
-
-        $this->template->data = $recent;
-    }
-
     public function actionDefault($color = 'black') {
         $f = new FlickrModel("eb01c6c4e23a0f036988692a7f42dd14");
 
